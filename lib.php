09b4cd7e7b38c4cf4b6ef16012f2f3d4afe6f106
winnidat <thomas.winkler@wunderbyte.at>
2024-08-27 23:26:07 +0200

    new color

--- lib.php
+++ lib.php
@@ -89,5 +89,5 @@ function qbank_nocorrectanswer_init() {
 function qbank_nocorrectanswer_after_config() {
     global $CFG;
     qbank_nocorrectanswer_init();
-    $CFG->chart_colorset = ['#dc3c28', '#32b400'];
+    $CFG->chart_colorset = ['#32b400', '#vdc3c28'];
 }
